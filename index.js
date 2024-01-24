e95264b39b0490b58b49f8baf10603696695fb4a
Daniel Turiño Anido <danieltani@servicioexterno.inditex.com>
2024-01-24 09:19:27 +0100

    Add debug to console

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const fs = require('fs-extra');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
 //const createArtifact = require('./utils/create-artifact');
 //const deploy = require('./utils/deploy');
 
